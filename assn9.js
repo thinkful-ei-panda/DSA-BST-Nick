50604d8a490ac7d9db85df6b1ba9b8d6feab553d
nickthorpe71 <nickthorpe71@gmail.com>
2020-08-25 19:59:42 -0500

    Q 9 attempt 2

--- assn9.js
+++ assn9.js
@@ -1,28 +1,29 @@
-import BinartSearchTree from './BST.js';
-
 function main() {
-  const nums1 = [3, 1, 4, 6, 9, 2, 5, 7];
-  const nums2 = [3, 1, 4, 6, 9, 2, 5, 7];
+  const nums1 = [3, 5, 4, 6, 1, 0, 2];
+  const nums2 = [3, 1, 5, 2, 4, 6, 0];
 
-  console.log(willBeEqualBST(nums1, nums2, nums1.length, 0, 0, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER,));
+  console.log(willBeEqualBST(nums1, nums2));
 }
 
-function willBeEqualBST(arr1, arr2, n, i1, i2, min, max) {
-  let j = 0, k = 0;
-
-  for (j = i1; j < n; j++)
-    if (arr1[j] > min && arr1[j] < max)
-      break;
-  for (k = i2; k < n; k++)
-    if (arr2[k] > min && arr2 < max)
-      break;
-
-  if (j === n && k === n)//If the parent element is leaf in both arrays
-    return true;
-  if ((j === n) ^ (k === n) || arr1[j] !== arr2[k])
+function willBeEqualBST(arr1, arr2) {
+  if (arr1.length !== arr2.length)
     return false;
 
-  return willBeEqualBST(arr1, arr2, n, j + 1, k + 1, arr1[j], max) && willBeEqualBST(arr1, arr2, n, j + 1, k + 1, min, arr1[j]);
+  if (arr1.length === 0)
+    return true;
+
+  if (arr1[0] !== arr2[0])
+    return false;
+
+  let arrLeft1 = [], arrRight1 = [], arrLeft2 = [], arrRight2 = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    (arr1[i] < arr1[0]) ? arrLeft1.push(arr1[i]) : arrRight1.push(arr1[i]);
+
+    (arr2[i] < arr2[0]) ? arrLeft2.push(arr2[i]) : arrRight2.push(arr2[i]);
+  }
+
+  return willBeEqualBST(arrLeft1, arrLeft2) && willBeEqualBST(arrRight1, arrRight2);
 }
 
 main();
